accbdbf46d6e139c889d3af59820e4cfa6904ea5
ForrestWeston <Forrest.Weston@gmail.com>
2015-04-16 11:29:40 -0700

    Def functions

---
+++ glue/Glue.php
@@ -0,0 +1,39 @@
+<?php
+/*
+mvc_build_url(CONTROLLER, ACTION, PARAMS [optional])
+Similar to CodeIgniter's site_url function. This function will return a string containing a URL to the
+specified controller and action.
+
+mvc_build_style_url(CSS)
+This function will return a string containing a URL to the specified CSS file located in /content/style.
+
+mvc_build_script_url(SCRIPT)
+This function will return a string containing a URL to the specified JavaScript file located in
+/content/scripts.
+
+mvc_redirect(CONTROLLER, ACTION, PARAMS [optional])
+Redirect's the user's browser to the supplied controller and action
+*/
+class glue
+{
+	public function mvc_build_url($controller, $action, $params)
+	{
+	
+	}	
+
+	public function mvc_build_style_url($css)
+	{
+	
+	}
+
+	public function mvc_build_script_url($script)
+	{
+		
+	}
+	
+	public function mvc_redirect($controller, $action, $params)
+	{
+			
+	}
+	
+}
